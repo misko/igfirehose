dc9912c47097e277f7fcea86dc42cf860b9d4483
Ubuntu <ubuntu@ip-172-31-35-51.us-east-2.compute.internal>
2018-06-05 00:38:46 +0000

    updates

--- igf-py/algo_online.py
+++ igf-py/algo_online.py
@@ -12,14 +12,28 @@ parser = argparse.ArgumentParser(
 parser.add_argument("-s","--seed", help="seed",type=str,required=True)
 parser.add_argument("-n","--number", help="hashtag",type=int, default=10000)
 parser.add_argument("-hn","--hashtag-number", help="hashtag",type=int, default=10000)
-parser.add_argument("-tn","--tag-number", help="hashtag",type=int, default=10000)
+parser.add_argument("-tn","--tag-back-number", help="hashtag",type=int, default=10000)
 parser.add_argument("-c", "--config", help="config file",required=True,type=str)
 parser.add_argument("-b", "--tag-back", help="tagback",type=float,default=0.005)
 parser.add_argument("-o", "--online", help="tagback",type=int,default=20)
 parser.add_argument("-t", "--threshold", help="t",type=int,default=0.0002)
+parser.add_argument("-p", "--pool-size", help="pool size",type=int,default=16)
+parser.add_argument("-d", "--out-dir", help="out-dir",type=str,default='algo_out')
 args = parser.parse_args()
+import errno    
+import os
 
+#https://stackoverflow.com/questions/600268/mkdir-p-functionality-in-python
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
 
+mkdir_p(args.out_dir)
 igf = IGFirehose(args.config)
 
 #first get hashtags and lets check frequencies
@@ -39,25 +53,41 @@ def get_freq():
     s.sort(reverse=True)
     return s
 
+
+
+def update_helper(a):
+    tagAs,tagB=a
+    igf = IGFirehose(args.config)
+    return igf.get_co_p(tagAs,tagB,n=args.number)
+    
+
 def update_matrix(N):
     #make sure we have all needed entries
+    todo=[]
     for tagB in N:
 	if tagB not in M:
-		M[tagB]={}
-		r=igf.get_co_p(N,tagB,n=args.number)
-		#print "BIG CALL",tagB
-		for idx in xrange(len(N)):
-			M[tagB][N[idx]]=r[idx]
+		todo.append((N,tagB))
+		#M[tagB]={}
+		#r=igf.get_co_p(N,tagB,n=args.number)
+		#for idx in xrange(len(N)):
+		#	M[tagB][N[idx]]=r[idx]
 	else:
 		for tagA in N:
 			if tagA not in M[tagB]:
-				# M[X][Y] = pr(X | Y )
-				# co_p ( A , B ) = pr (A | B)
-				#print "SMALL CALL",tagA,tagB
-				M[tagB][tagA]=igf.get_co_p([tagA],tagB,n=args.number)[0]
+				todo.append(([tagA],tagB))
+				#M[tagB][tagA]=igf.get_co_p([tagA],tagB,n=args.number)[0]
+    rmap=p.map(update_helper, todo)
+    for i in xrange(len(todo)):
+        tagAs,tagB = todo[i] #input
+        r=rmap[i] #output
+        if tagB not in M:
+            M[tagB]={}
+        for j in xrange(len(tagAs)):
+    	    M[tagB][tagAs[j]]=r[j]
+	
 
 def print_m(M,N):
-    print ",".join( ["X"] + N)
+    s=[",".join( ["X"] + N)]
     for i in N: # pick the row
         row=[]
         for j in N: # for each row
@@ -65,12 +95,12 @@ def print_m(M,N):
                 row.append(0)
 		continue
 	    row.append(M[i][j])
-        print ",".join([i]+["%0.3e" % x for x in row]) 
+        s.append(",".join([i]+["%0.3e" % x for x in row]) )
+    return "\n".join(s) 
 	
 # X is cotag matrix
 # N is set of unused nodes
 def prs(M,N):
-    print_m(M,N)
     r={}
     for i in N: # pick the col
         r[i]=0
@@ -83,11 +113,11 @@ def prs(M,N):
         r[i]/=norm
     return r
 
+p = Pool(args.pool_size)
 
 freqs=get_freq()
 
 
-p = Pool(16)
 M={}
 N=[args.seed] #,'poodle','goldenretriever']
 S=[]
@@ -95,7 +125,7 @@ for _,tag in freqs:
 	if tag==args.seed:
 		continue
 	#check the tag back
-	tb=igf.get_co_p([args.seed],tag,n=args.tag_number)[0]
+	tb=igf.get_co_p([args.seed],tag,n=args.tag_back_number)[0]
 	if tb<args.tag_back:
 		print "Dropping",tag,"because of tagback",tb,"<",args.tag_back
 		continue
@@ -108,6 +138,13 @@ for _,tag in freqs:
 		argmax=vs[0]
 		print "WINNER",argmax
 		if argmax[0]>args.threshold:
+			matrix=print_m(M,N)
+			f=open(args.out_dir+'/'+argmax[1]+'.txt','w')
+			f.write(str(argmax)+'\n')
+			f.write(",".join(N)+'\n')
+			f.write(",".join(S)+'\n')
+			f.write(matrix+'\n')
+			f.close()
 			S.append(argmax[1])
 			N.remove(argmax[1])
 	
