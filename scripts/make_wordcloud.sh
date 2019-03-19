3666eddc932a62148571ff89c32ff4487de96673
Ubuntu <ubuntu@ip-172-31-43-100.us-east-2.compute.internal>
2019-03-19 20:18:48 +0000

    fix make wordcloud

--- scripts/make_wordcloud.sh
+++ scripts/make_wordcloud.sh
@@ -7,5 +7,5 @@ fi
 
 tag=$1
 root=$2
-
-python $root/igf-py/query_hashtags.py -c $root/igf-py/igf.conf -t $tag -n 2000 | sed 's/^#//g' | wordcloud_cli --text - --background white --imagefile $root/igf-py/static/wordclouds/${line}.png --no_collocations --height 300 --width 600
+echo $root/igf-py/static/wordclouds/${tag}.png
+python $root/igf-py/query_hashtags.py -c $root/igf-py/igf.conf -t $tag -n 2000 | sed 's/^#//g' | wordcloud_cli --text - --background white --imagefile $root/igf-py/static/wordclouds/${tag}.png --no_collocations --height 300 --width 600
